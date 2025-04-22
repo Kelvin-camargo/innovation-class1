ad4aa115d7899264828a909e77283bcb5b3d2e3d
kelvin.M.camargo <kelvin.m.camargo@gmail.com>
2025-04-21 21:53:57 -0300

    terminado o carrosel da tela section2

---
+++ src/script/section2/carousel.js
@@ -0,0 +1,63 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    spaceBetween: 10,
+   
+    slidesPerView: 5,
+    slidesPerGroup: 1,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 2,
+      
+      },
+
+      "@1.50": {
+        slidesPerView: 5,
+        spaceBetween: -5,
+      },
+    }
+  });
+
+
+
+  /*var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  */
